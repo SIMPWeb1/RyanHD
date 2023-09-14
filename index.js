735e3b193bb3f2dbcbb113541eb52828d8392735
Phong <phong@sss.local>
2023-09-14 19:00:32 +0700

    Done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function copyAddress(params) {
-    navigator.clipboard.writeText("test address");
+    navigator.clipboard.writeText("0x0d1Ce10163579680876C6Fb93586c10a3e44F3ed");
 }
 
 function onLinkClick(section) {
