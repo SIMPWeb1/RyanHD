c9b5367a5a6c278aba0db035513353b6a33f370b
Phong <phong@sss.local>
2023-09-14 16:44:58 +0700

    Done scroll

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function copyAddress(params) {
     navigator.clipboard.writeText("test address");
 }
+
+function onLinkClick(section) {
+    document.getElementById(section).scrollIntoView({ behavior: 'smooth' })
+    // will scroll to 4th h3 element
+  }
